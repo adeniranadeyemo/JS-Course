c224ae9000f46119c18f5eb86de7a9542d84bd36
Adeniran Adeyemo <89475955+adeniranadeyemo@users.noreply.github.com>
2023-01-07 18:26:02 +0100

    Added a function that calculated the average of the 'totals' array.

--- script.js
+++ script.js
@@ -668,7 +668,7 @@ const calcTip = function (bill) {
   return tip;
 };
 
-for (let i = 0; i <= bills.length - 1; i++) {
+for (let i = 0; i < bills.length; i++) {
   tips.push(calcTip(bills[i]));
   totals.push(bills[i] + tips[i]);
   console.log(`Bill: ${bills[i]}. Tip: ${tips[i]}. Total: ${totals[i]}`);
@@ -676,3 +676,12 @@ for (let i = 0; i <= bills.length - 1; i++) {
 }
 console.log(tips);
 console.log(totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage(totals));

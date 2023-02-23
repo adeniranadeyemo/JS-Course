f26ea05bf98a7d711da1823bf86a93de966c4ad0
Adeniran Adeyemo <89475955+adeniranadeyemo@users.noreply.github.com>
2023-02-23 19:30:53 +0100

    Hoisting and TDZ

--- za-complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ za-complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,69 @@
 'use strict';
+// function calcAge(birthyear) {
+//     const age = 2023 - birthyear;
+
+//     function printAge() {
+//         let output = `${firstName}, you are ${age}, born in year ${birthyear}.`;
+//         console.log(output);
+
+//         const str = `Oh! You're a millenial, ${firstName}.`;
+//         console.log(str);
+
+//     if (birthyear >= 1981 && birthyear <= 1986) {
+//       var millenial = true;
+
+//       // Creating NEW variables with same name as outer scope's variable
+//       const firstName = 'Steven';
+//       const age = 2037 - birthyear;
+
+//       // Reassigning outer scope's variable
+//       output = `${firstName}, you are ${age}, born in year ${birthyear}. Interesting!`;
+//       console.log(output);
+
+//       const str = `Oh! You're a millenial, ${firstName}.`;
+//       console.log(str);
+
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       return age;
+//     }
+//     // console.log(str);
+//     // console.log(add(2, 4));
+//     console.log(output);
+//     console.log(millenial);
+//   }
+//   printAge();
+
+//   return age;
+// }
+
+// const firstName = 'Lagbaja';
+// // console.log(calcAge(2000));
+// calcAge(1983);
+
+// const myName = 'Niran';
+
+// if (myName === 'Niran') {
+//   const age = 2023 - 2000;
+//   console.log(myName);
+//   console.log(age);
+//   console.log(`Niran is a ${job}`);
+//   const job = `Designer/Developer`;
+// }
+
+// Hoisting with variables
+console.log(me);
+console.log(birthyear);
+console.log(job);
+
+var me = 'Niran';
+let job = 'Dev';
+const birthyear = 2000;
+
+// Hoisting with Functions
+function addDecl(a, b) {
+  return a+b;
+}
+
+// const addEcp

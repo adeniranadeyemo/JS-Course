e9302ed4eacf56f98feeff3c119d8888e8b0098e
Adeniran Adeyemo <89475955+adeniranadeyemo@users.noreply.github.com>
2023-02-24 18:58:45 +0100

    Hoisting.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 // function calcAge(birthyear) {
 //     const age = 2023 - birthyear;
 
@@ -53,17 +53,56 @@
 // }
 
 // Hoisting with variables
-console.log(me);
-console.log(birthyear);
-console.log(job);
+// console.log(me);
+// console.log(birthyear);
+// console.log(job);
 
-var me = 'Niran';
-let job = 'Dev';
-const birthyear = 2000;
+// var me = 'Niran';
+// let job = 'Dev';
+// const birthyear = 2000;
 
 // Hoisting with Functions
-function addDecl(a, b) {
-  return a+b;
-}
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow);
 
-// const addEcp
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+//Example
+
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+// //var always returns undefined
+
+// function deleteShoppingCart() {
+//   console.log(`All Products deleted!`);
+// }
+
+// //Best Practice
+
+// let numbProducts = 10;
+
+// function delShoppingCart() {
+//   console.log(`All Products deleted!`);
+// }
+
+// if (!numbProducts) delShoppingCart();
+
+// var usually creates variables as property of the window object.
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+

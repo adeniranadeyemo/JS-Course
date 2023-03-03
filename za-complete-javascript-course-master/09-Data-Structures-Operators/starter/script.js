c3200e1e5657894e64e47527a9a7ea31a9daea39
adeniranadeyemo <niranadeniran1@gmail.com>
2023-03-03 18:06:45 +0100

    Destructuring Arrays

--- za-complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ za-complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,34 @@ const restaurant = {
     },
   },
 };
+
+const arr = [1, 2, 5];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// console.log(a, b, c);
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//Nested destructuring
+const nested = [2, 4, [5, 6]];
+// let [first, , third] = nested;
+// console.log(first, third);
+
+let [first, , [third, fourth]] = nested;
+console.log(first, third, fourth);
+
+//Default values
+const [p = 1, q = 2, r = 3] = [8, 9];
+console.log(p, q, r);

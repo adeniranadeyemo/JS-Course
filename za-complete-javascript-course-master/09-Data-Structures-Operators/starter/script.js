be714298242a7509bd40c2ab00dff93f5d9f976b
adeniranadeyemo <niranadeniran1@gmail.com>
2023-04-07 16:22:12 +0100

    Sets and Maps

--- za-complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ za-complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -399,56 +399,56 @@ const restaurant = {
 // console.log(guestCorrect);
 
 // CHALLENGE
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnabry',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-  printGoals: function (...playersThatScored) {
-    let sum = '';
-    for (let i = 0; i < playersThatScored.length; i++) {
-      sum += playersThatScored[i] + ' ';
-    }
-    // console.log(...playersThatScored);
-    // console.log(
-    //   `${playersThatScored} scored ${playersThatScored.length} goals.`
-    // );
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnabry',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+//   printGoals: function (...playersThatScored) {
+//     let sum = '';
+//     for (let i = 0; i < playersThatScored.length; i++) {
+//       sum += playersThatScored[i] + ' ';
+//     }
+// console.log(...playersThatScored);
+// console.log(
+//   `${playersThatScored} scored ${playersThatScored.length} goals.`
+// );
+//   },
+// };
 
 // 1
 // const [players1, players2] = game.players;
@@ -484,13 +484,12 @@ const game = {
 // CHALLENGE TWO
 // 1
 // FOR OF LOOP
-let goalScorers = [...game.scored];
-let goalEntries = Object.entries(goalScorers);
+// let goalEntries = Object.entries(game.scored);
 
-for (const [i, scorer] of goalEntries) {
-  console.log(`Goal ${Number(i) + 1}: ${scorer}`);
-}
-console.log('');
+// for (const [i, scorer] of goalEntries) {
+//   console.log(`Goal ${Number(i) + 1}: ${scorer}`);
+// }
+// console.log('');
 
 //OR
 // FOR IN LOOP
@@ -500,76 +499,221 @@ console.log('');
 // }
 
 // 2
-let odds = Object.values(game.odds);
-let sum = 0;
-let average = 0;
-for (const odd of odds) {
-  sum += odd;
-  average = sum / odds.length;
-}
-console.log(average);
-console.log('');
+// let odds = Object.values(game.odds);
+// let sum = 0;
+// let average = 0;
+// for (const odd of odds) {
+//   sum += odd;
+//   average = sum / odds.length;
+// }
+// console.log(average);
+// console.log('');
 
 // 3
-const {
-  team1,
-  team2,
-  odds: { team1: t1, x, team2: t2 },
-} = game;
+// const {
+//   team1,
+//   team2,
+//   odds: { team1: t1, x, team2: t2 },
+// } = game;
 
-console.log(
-  `${team1}'s odd of vitory: ${t1}. Odds of draw: ${x}. ${team2}'s odd of victory: ${t2}.`
-);
-console.log('');
+// console.log(
+//   `${team1}'s odd of victory: ${t1}. Odds of draw: ${x}. ${team2}'s odd of victory: ${t2}.`
+// );
+
+// let oddsEntries = Object.entries(game.odds);
+
+// for (const [team, odd] of oddsEntries) {
+//   const teamStr = team === 'x' ? 'draw:' : `Victory ${game[team]}:`;
+//   console.log(`Odd of ${teamStr} ${odd}`);
+// }
+
+// console.log('');
 
 // BONUS
-let gscorers = { ...game.scored };
+// const scorers = {};
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+// }
 
-const lewa = 0;
-const gnabry = 1;
-const hummels = 3;
+// console.log(scorers);
 
-const lewaValue = gscorers[lewa];
-const gnabryValue = gscorers[gnabry];
-const hummelsValue = gscorers[hummels];
+// let gscorers = { ...game.scored };
 
-const lewaCount = Object.values(gscorers).reduce(
-  (accumulator, currentValue) => {
-    if (currentValue === 'Lewandowski') {
-      return accumulator + 1;
-    } else {
-      return accumulator;
-    }
-  },
-  3
-);
+// const lewa = 0;
+// const gnabry = 1;
+// const hummels = 3;
 
-const gnabryCount = Object.values(gscorers).reduce(
-  (accumulator, currentValue) => {
-    if (currentValue === 'Gnabry') {
-      return accumulator + 1;
-    } else {
-      return accumulator;
-    }
-  },
-  0
-);
+// const lewaValue = gscorers[lewa];
+// const gnabryValue = gscorers[gnabry];
+// const hummelsValue = gscorers[hummels];
 
-const hummelsCount = Object.values(gscorers).reduce(
-  (accumulator, currentValue) => {
-    if (currentValue === 'Hummels') {
-      return accumulator + 1;
-    } else {
-      return accumulator;
-    }
-  },
-  0
-);
+// const lewaCount = Object.values(gscorers).reduce(
+//   (accumulator, currentValue) => {
+//     if (currentValue === 'Lewandowski') {
+//       return accumulator + 1;
+//     } else {
+//       return accumulator;
+//     }
+//   },
+//   0
+// );
 
-const scorers = {
-  [gnabryValue]: gnabryCount,
-  [hummelsValue]: hummelsCount,
-  [lewaValue]: lewaCount,
-};
+// const gnabryCount = Object.values(gscorers).reduce(
+//   (accumulator, currentValue) => {
+//     if (currentValue === 'Gnabry') {
+//       return accumulator + 1;
+//     } else {
+//       return accumulator;
+//     }
+//   },
+//   0
+// );
 
-console.log(scorers);
+// const hummelsCount = Object.values(gscorers).reduce(
+//   (accumulator, currentValue) => {
+//     if (currentValue === 'Hummels') {
+//       return accumulator + 1;
+//     } else {
+//       return accumulator;
+//     }
+//   },
+//   0
+// );
+
+// const scorers = {
+//   [gnabryValue]: gnabryCount,
+//   [hummelsValue]: hummelsCount,
+//   [lewaValue]: lewaCount,
+// };
+
+// console.log(scorers);
+
+///////////////////////////////////////////////////////
+// SETS
+// const ordersSet = new Set([
+//   'Pizza',
+//   'Pasta',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+//   'Pounded yam',
+//   'Eba',
+//   'Beans',
+// ]);
+
+// console.log(ordersSet);
+
+// console.log(new Set('Niran'));
+
+// console.log(ordersSet.size);
+
+// console.log(ordersSet.has('Pizza'));
+// console.log(ordersSet.has('Spaghetti'));
+
+// ordersSet.add('Garlic Bread');
+// ordersSet.add('Garlic Bread');
+// ordersSet.add('Porridge');
+
+// ordersSet.delete('Risotto');
+// ordersSet.clear();
+// console.log(ordersSet);
+
+// for (const order of ordersSet) {
+//   console.log(order);
+// }
+
+//Example
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+
+// console.log(new Set(staff).size);
+
+////////////////////////////////////////////////////////
+// MAPS
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// rest.set(2, 'Lisbon, Portugal');
+// console.log(rest);
+// console.log(rest.set(3, 'Ibadan, Nigeria'));
+
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :(');
+// console.log(rest);
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 24;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// console.log(rest.has('latitude'));
+// rest.delete(2);
+
+// rest.set([1, 2], 'Test');
+
+// const arr = [3, 4];
+// rest.set(arr, 'Quest');
+
+// console.log(rest.get([1, 2]));
+// console.log(rest.get(arr));
+
+// rest.set(document.querySelector('h1'), 'heading');
+// console.log(rest);
+// console.log(rest.size);
+// // rest.clear();
+// console.log(rest);
+
+////////
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['Correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Converting object to maps
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// Quiz app
+// alert(question.get('question'));
+
+console.log(question.get('question'));
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('Correct') === answer));
+
+// if (answer === question.get('Correct')) {
+//   console.log(question.get(true));
+// } else {
+//   console.log(question.get(false));
+// }
+
+// Converting map to array
+// console.log(...question);
+// console.log([...question.keys()]);
+// console.log([...question.values()]);

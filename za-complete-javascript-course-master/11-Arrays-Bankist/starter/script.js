1c33fe842ca85e6da6f55fbef3a676e9cfecfde2
adeniranadeyemo <niranadeniran1@gmail.com>
2023-07-13 21:50:19 +0100

    First array methods challenge

--- za-complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ za-complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -87,7 +87,26 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+// CHALLENGE ////////////////////////////////////
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
 
+const checkDogs = function (array1, array2) {
+  let dogsJuliaCopy = [...array1];
+  let array3 = dogsJuliaCopy.slice(1, 3);
+
+  const allDogs = [...array3, ...array2];
+  console.log(allDogs);
+
+  allDogs.forEach(function (dog, i) {
+    dog >= 3
+      ? console.log(`Dog number ${i + 1} is an adult and is ${dog} years old.`)
+      : console.log(
+          `Dog number ${i + 1} is ${dog} years old and still a puppy.`
+        );
+  });
+};
+checkDogs(dogsJulia, dogsKate);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -101,6 +120,7 @@ displayMovements(account1.movements);
 // console.log(array.slice(2));
 // console.log(array.slice(2, 4));
 // console.log(array.slice(-1));
+// console.log(array);
 // console.log(array.slice(-2));
 // console.log(array.slice(1, -2));
 // console.log(array.slice());

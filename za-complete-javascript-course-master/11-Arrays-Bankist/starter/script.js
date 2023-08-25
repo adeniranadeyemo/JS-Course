4b813e3ad3610317767e70375d3634d521aea63f
adeniranadeyemo <niranadeniran1@gmail.com>
2023-08-25 21:29:38 +0100

    Finished the last lesson on array methods. #11

--- za-complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ za-complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -622,6 +622,23 @@ const { deposits, withdrawals } = accounts
 
 console.log(deposits, withdrawals);
 
+// 4
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title.'));
+console.log(convertTitleCase('this is a LONG title but not too long.'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE.'));
 /////////////////////////////////////////////////////
 /////////////////////////////////////////////////////
 /////////////////////////////////////////////// PERSONAL CHALLENGE ON MAP callback function, BIND and FOREACH
